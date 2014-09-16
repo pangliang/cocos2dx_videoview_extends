e08c4d3e3bd7a6a2875b0184e073cca6869ebcc8
liang8305 <418094911@qq.com>
2014-09-16 20:09:09 +0800

    android视频播放中退出桌面再进入崩溃

--- frameworks/runtime-src/proj.android/src/cn/sharedream/game/VideoView.java
+++ frameworks/runtime-src/proj.android/src/cn/sharedream/game/VideoView.java
@@ -2,6 +2,7 @@ package cn.sharedream.game;
 
 import java.io.IOException;
 
+import org.cocos2dx.lib.Cocos2dxLuaJavaBridge;
 import org.cocos2dx.lua.AppActivity;
 
 import android.app.Activity;
@@ -14,113 +15,105 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * 
- * @author Yichou
- *
- * create data:2013-4-22 22:19:49
- */
-public class VideoView extends SurfaceView implements 
-			SurfaceHolder.Callback, 
-			View.OnTouchListener, 
-			MediaPlayer.OnPreparedListener, 
-			MediaPlayer.OnErrorListener, 
-			MediaPlayer.OnInfoListener,
-			MediaPlayer.OnCompletionListener {
+public class VideoView extends SurfaceView implements SurfaceHolder.Callback,
+		View.OnTouchListener,
+		MediaPlayer.OnErrorListener, MediaPlayer.OnInfoListener,
+		MediaPlayer.OnCompletionListener {
 	private static final String TAG = "VideoView";
-	
+
 	private MediaPlayer mPlayer; // MediaPlayer对象
-	private Activity gameActivity;
 	private AssetFileDescriptor fd;
-	private boolean surfaceCreated;
-	ViewGroup group;
 	int luaOnFinishCallback;
-	
+	private static long videoStartTime = 0;
+	private static long videoFinishLimit = 5 *1000;
+	private boolean surfaceCreated = false;
 
 	public VideoView(Activity context) {
+		
 		super(context);
-
-		this.gameActivity = context;
-		group = (ViewGroup)context.getWindow().getDecorView();
+		Log.i(TAG, "new VideoView");
 
 		final SurfaceHolder holder = getHolder();
 		holder.addCallback(this); // 设置回调接口
 		setOnTouchListener(this);
 
-		mPlayer = new MediaPlayer();
-		mPlayer.setScreenOnWhilePlaying(true);
-
-		mPlayer.setOnPreparedListener(this);
-		mPlayer.setOnCompletionListener(this);
-		mPlayer.setOnErrorListener(this);
-		mPlayer.setOnInfoListener(this);
+		
 	}
-	
+
 	public void setVideo(AssetFileDescriptor fd) {
 		this.fd = fd;
 		try {
-			mPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
+			mPlayer = new MediaPlayer();
+			mPlayer.setScreenOnWhilePlaying(true);
+
+			mPlayer.setOnCompletionListener(this);
+			mPlayer.setOnErrorListener(this);
+			mPlayer.setOnInfoListener(this);
+			mPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(),
+					fd.getLength());
+			mPlayer.prepare();
+			posttion = 0;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
 	}
 
 	@Override
 	public void surfaceCreated(final SurfaceHolder holder) {
 		Log.i(TAG, "surfaceCreated");
-
-		surfaceCreated = true;
-
-		mPlayer.setDisplay(holder); // 指定SurfaceHolder
 		try {
-			mPlayer.prepare();
-		} catch (Exception e1) {
+			
+			int wWidth = getWidth();
+			int wHeight = getHeight();
+
+			/* 获得视频宽长 */
+			int vWidth = mPlayer.getVideoWidth();
+			int vHeight = mPlayer.getVideoHeight();
+
+			/* 最适屏幕 */
+			float wRatio = (float) vWidth / (float) wWidth; // 宽度比
+			float hRatio = (float) vHeight / (float) wHeight; // 高度比
+			float ratio = Math.max(wRatio, hRatio); // 较大的比
+			vWidth = (int) Math.ceil((float) vWidth / ratio); // 新视频宽度
+			vHeight = (int) Math.ceil((float) vHeight / ratio); // 新视频高度
+
+			// 改变SurfaceHolder大小
+			holder.setFixedSize(vWidth, vHeight);
+			
+			mPlayer.setDisplay(holder); // 指定SurfaceHolder
+			mPlayer.seekTo(posttion);
+			mPlayer.start();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			onVideoFinish();
 		}
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
-		Log.i(TAG, "surfaceDestroyed");
-		surfaceCreated = false;
 		
-		if(mPlayer != null){
-			mPlayer.stop();
-			mPlayer.reset();
+		try {
+			Log.i(TAG, "surfaceDestroyed");
+			if (mPlayer != null){
+				posttion = mPlayer.getCurrentPosition();
+				mPlayer.pause();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
 	}
 
-	@Override
-	public void onPrepared(MediaPlayer player) {
-		Log.i(TAG, "onPrepared");
-
-		int wWidth = getWidth();
-		int wHeight = getHeight();
-
-		/* 获得视频宽长 */
-		int vWidth = mPlayer.getVideoWidth();
-		int vHeight = mPlayer.getVideoHeight();
-
-		/* 最适屏幕 */
-		float wRatio = (float) vWidth / (float) wWidth; // 宽度比
-		float hRatio = (float) vHeight / (float) wHeight; // 高度比
-		float ratio = Math.max(wRatio, hRatio); // 较大的比
-		vWidth = (int) Math.ceil((float) vWidth / ratio); // 新视频宽度
-		vHeight = (int) Math.ceil((float) vHeight / ratio); // 新视频高度
-
-		// 改变SurfaceHolder大小
-		getHolder().setFixedSize(vWidth, vHeight);
-		mPlayer.seekTo(posttion);
-		mPlayer.start();
-	}
 
 	@Override
 	public void onCompletion(MediaPlayer mp) {
 		Log.i(TAG, "onCompletion");
-
 		onVideoFinish();
 	}
 
@@ -131,83 +124,84 @@ public class VideoView extends SurfaceView implements
 
 	@Override
 	public boolean onError(MediaPlayer mp, int what, int extra) {
-		return true;
+		System.out.println("play error:" + what + "," + extra);
+		// return false 会走到onCompletion处理
+		return false;
 	}
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			//onVideoFinish();
+			long now =System.currentTimeMillis();
+			System.out.println("key down: nowSecns=" + now + ",limitSecns"
+					+ (videoStartTime + videoFinishLimit));
+			if (now > (videoStartTime + videoFinishLimit)) {
+				onVideoFinish();
+			}
 		}
 		return true;
 	}
-	
+
 	int posttion;
-	public void pause() {
-		posttion = mPlayer.getCurrentPosition();
-		mPlayer.pause();
-	}
-	
-	/**
-	 * 暂停的时候，系统会销毁 SurfaceView ，所以在resume的时候相对于重新设置MediaPlayer
-	 */
-	public void resume() {
-		if(surfaceCreated){
-			mPlayer.seekTo(posttion);
-			mPlayer.start();
-		}else {
-			try {
-				if (fd != null) {
-					mPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
-				}
-			} catch (Exception e) {
+
+	public void onVideoFinish() {
+		Log.i(TAG, "onVideoFinish");
+		try {
+			if (mPlayer != null){
+				mPlayer.stop();
+				mPlayer.release();
+				mPlayer = null;
 			}
-		}
-	}
-	
-	public void onVideoFinish()
-	{
-		if(mPlayer == null)
-			return;
-		
-		mPlayer.stop();
-		mPlayer.release();
-		mPlayer = null;
-		if (fd != null) {
-			try {
+			if (fd != null) {
 				fd.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+				fd = null;
 			}
-			fd = null;
+		} catch (Exception e) {
+			Log.i(TAG, "onVideoFinish error");
+			e.printStackTrace();
 		}
-		
-		final AppActivity instance = AppActivity.instance;
-		instance.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				ViewGroup group = (ViewGroup)instance.getWindow().getDecorView();
-				group.removeView(VideoView.this);
-			}
-		});
-		
-		if (luaOnFinishCallback != 0){
-			instance.runOnGLThread(new Runnable() {
+		try {
+			final AppActivity instance = AppActivity.instance;
+			instance.runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					doLuaFinishCallback(luaOnFinishCallback);
+					try{
+						ViewGroup group = (ViewGroup) instance.getWindow()
+								.getDecorView();
+						group.removeView(VideoView.this);
+					}catch(Exception e){
+						e.printStackTrace();
+					}
 				}
 			});
-		}
 			
+			if (luaOnFinishCallback != 0) {
+				instance.runOnGLThread(new Runnable() {
+					@Override
+					public void run() {
+						try{
+							Cocos2dxLuaJavaBridge.callLuaFunctionWithString(
+									luaOnFinishCallback, "FINISH");
+							Cocos2dxLuaJavaBridge
+									.releaseLuaFunction(luaOnFinishCallback);
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void setLuaOnFinishCallback(int luaOnFinishCallback) {
 		this.luaOnFinishCallback = luaOnFinishCallback;
 	}
 
-	public static void playVideo(final String name,final int luaCallback) {
+	public static void playVideo(final String name, final int luaCallback) {
 		final AppActivity instance = AppActivity.instance;
+		videoStartTime = System.currentTimeMillis();
 		if (instance != null) {
 			instance.runOnUiThread(new Runnable() {
 				@Override
@@ -215,13 +209,16 @@ public class VideoView extends SurfaceView implements
 					VideoView videoView = new VideoView(instance);
 					videoView.setLuaOnFinishCallback(luaCallback);
 					try {
-						AssetFileDescriptor afd = instance.getAssets().openFd(name);
+						AssetFileDescriptor afd = instance.getAssets().openFd(
+								name);
 						videoView.setVideo(afd);
-						ViewGroup group = (ViewGroup)instance.getWindow().getDecorView();
+						ViewGroup group = (ViewGroup) instance.getWindow()
+								.getDecorView();
 						group.addView(videoView);
 						videoView.setZOrderMediaOverlay(true);
 					} catch (IOException e) {
 						e.printStackTrace();
+						videoView.onVideoFinish();
 					}
 				}
 			});
