26e9247e506397a11fb99fc532997260e3e1d63e
liang8305 <418094911@qq.com>
2014-11-13 16:44:08 +0800

    1.ios 支持点击结束播放 2.bug fix, ios8视频播放横竖屏问题

--- frameworks/runtime-src/proj.ios_mac/ios/VideoViewIOSImp.h
+++ frameworks/runtime-src/proj.ios_mac/ios/VideoViewIOSImp.h
@@ -2,10 +2,14 @@
 // Created by liangwei on 14-7-21.
 //
 
+#import <MediaPlayer/MediaPlayer.h>
+
 @interface VideoViewIOSImp : NSObject{
     void (*movieFinishedCallback)(int);
     int luaFuncID;
+    MPMoviePlayerController *player;
 }
+-(void) handleTap:(UITapGestureRecognizer *)recognizer;
 -(void) playVideo:(const char*) filename luaFuncID:(int) funcID callback:(void (*)(int)) callback;
 -(void) doMovieFinishedNotification:(NSNotification*)aNotification;
 @end
--- frameworks/runtime-src/proj.ios_mac/ios/VideoViewIOSImp.mm
+++ frameworks/runtime-src/proj.ios_mac/ios/VideoViewIOSImp.mm
@@ -3,75 +3,87 @@
 //
 #include "VideoView.h"
 #import "VideoViewIOSImp.h"
-#import <MediaPlayer/MediaPlayer.h>;
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation VideoViewIOSImp
 
 -(void) doMovieFinishedNotification:(NSNotification*)aNotification{
-    MPMoviePlayerController *player = [aNotification object];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:player];
-
-    [player.view removeFromSuperview];
-    [player release];
-
+    MPMoviePlayerController *movieplayer = [aNotification object];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:movieplayer];
+    
+    [movieplayer.view removeFromSuperview];
+    [movieplayer release];
+    
     movieFinishedCallback(luaFuncID);
     [self release];
+    
+}
 
+- (void)handleTap:(UITapGestureRecognizer *)recognizer {
+    CCLOG("tap:%f",[player currentPlaybackTime]);
+    if([player currentPlaybackTime] > 5)
+        [player stop];
+    
 }
 
 -(void) playVideo:(const char*) filename luaFuncID:(int) funcID callback:(void (*)(int)) callback{
-
+    
     movieFinishedCallback = callback;
     luaFuncID = funcID;
-
-    MPMoviePlayerController *player2 = [ [ MPMoviePlayerController alloc]initWithContentURL:[NSURL fileURLWithPath:[NSString stringWithUTF8String:filename]]];
-
+    
+    player = [ [ MPMoviePlayerController alloc]initWithContentURL:[NSURL fileURLWithPath:[NSString stringWithUTF8String:filename]]];
+    
     bool showControls= false;
-
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(doMovieFinishedNotification:)
                                                  name:MPMoviePlayerPlaybackDidFinishNotification
-                                               object:player2];
-
+                                               object:player];
+    
     UIView* currentView = [UIApplication sharedApplication].keyWindow;
-
-    if ([player2 respondsToSelector:@selector(setFullscreen:animated:)]) {
-        player2.fullscreen = true;
-        player2.controlStyle = showControls ? MPMovieControlStyleFullscreen : MPMovieControlStyleNone;
-        CGRect bounds = [[UIScreen mainScreen] bounds];
-        [player2.view setBounds:CGRectMake(0, 0, bounds.size.height, bounds.size.width)];
-        [player2.view setCenter:CGPointMake(bounds.size.width/2, bounds.size.height/2)];
-        [player2.view setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-
-        player2.movieSourceType = MPMovieSourceTypeFile;
-        [player2 prepareToPlay];
-    } else {
-        // Use the old 2.0 style API
-        player2.movieControlMode = showControls ? MPMovieControlModeDefault : MPMovieControlModeHidden;
+    
+    player.fullscreen = true;
+    player.movieSourceType = MPMovieSourceTypeFile;
+    player.scalingMode = MPMovieScalingModeAspectFill;
+    player.controlStyle = showControls ? MPMovieControlStyleFullscreen : MPMovieControlStyleNone;
+    
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    [player.view setCenter:CGPointMake(bounds.size.width/2, bounds.size.height/2)];
+    
+    if (bounds.size.width>bounds.size.height){
+        [player.view setBounds:CGRectMake(0, 0, bounds.size.width, bounds.size.height)];
+    }else{
+        [player.view setBounds:CGRectMake(0, 0, bounds.size.height, bounds.size.width)];
+        [player.view setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
     }
-
-    if ([player2 respondsToSelector:@selector(view)]) {
-
-        [currentView addSubview:player2.view];
-        player2.shouldAutoplay=TRUE;
+    [player prepareToPlay];
+    
+    if ([player respondsToSelector:@selector(view)]) {
+        
+        [currentView addSubview:player.view];
+        player.shouldAutoplay=TRUE;
     }
-    [player2 play];
+    [player play];
+    
+    for (UIView * view in player.view.subviews) {
+        
+        UITapGestureRecognizer * recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+        [view addGestureRecognizer:recognizer];
+    }
+    
 }
 @end
 
 extern "C"  {
-void doLuaFinishCallback(int handle)
-{
-    LuaEngine::getInstance()->getLuaStack()->executeFunctionByHandler(handle, 0);
-}
+    void doLuaFinishCallback(int handle)
+    {
+        LuaEngine::getInstance()->getLuaStack()->executeFunctionByHandler(handle, 0);
+    }
 }
 
 void VideoView::playVideo(const char* filename,int funcID)
 {
-    std::string filePath = cocos2d::FileUtils::getInstance()->fullPathForFilename(filename);
+    std::string filePath = CCFileUtils::getInstance()->fullPathForFilename(filename);
     VideoViewIOSImp* ply =[[VideoViewIOSImp alloc] init];
     [ply playVideo : filePath.c_str() luaFuncID:funcID callback:doLuaFinishCallback];
 }
-
-
-
